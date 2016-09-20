b725eb61b9cf6dd82bc24bc011029134be8e9662
mikeadossi <mikeadossi@gmail.com>
2016-09-20 15:41:47 -0700

    setup database file

---
+++ database.js
@@ -0,0 +1,4 @@
+const databaseName = 'bookstore'
+const connectionString = process.env.DATABASE_URL || `postgres://${process.env.USER}@localhost:5432/${databaseName}`
+const pgp = require('pg-promise')()
+const db = pgp(connectionString)
