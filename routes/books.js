ccf23b9597ceddd8f142133433c614a1686cf0b6
Monica <monicaestellaw@gmail.com>
2016-09-21 16:41:21 -0700

    set up route and updated view thanks to majid

--- routes/books.js
+++ routes/books.js
@@ -10,13 +10,17 @@ router.get('/create', (req, res, next) => {
 router.post('/new', (req, res, next) => {
   const {title, author, image_url, description} = req.body
   db.createBook(title, author, image_url, description)
-    .then(book => res.redirect('/') )
-    .catch(error => { res.render('error', {error})
-  })
+    .then(book => res.redirect(`/books/${book.id}`) )
+    .catch(error => { res.render('error', {error})})
 })
 
 router.get('/:id', (req, res, next) => {
-  res.render('details', {book})
+  const book_id = req.params.id
+  db.getBookById(book_id)
+  .then(book => {
+    res.render('details', {book})
+  })
+  .catch(error => { res.render('error', {error})})
 })
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,9 @@ var db = require('../database');
 /* GET home page. */
 router.get('/', (request, response, next) => {
   db.getAllBooks()
-  .then(books => response.render('index', { books }));
-});
-
+  .then(books => {
+    response.render('index', {books})
+  })
+})
 
 module.exports = router;
