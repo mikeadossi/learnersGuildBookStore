c820feabc98fb7f97af8f5469f8ba68263aa6370
Monica <monicaestellaw@gmail.com>
2016-09-21 09:39:00 -0700

    set up form on what will be the create new book page

--- routes/books.js
+++ routes/books.js
@@ -4,6 +4,10 @@ var db = require('../database');
 
 
 //get the index/landing page
-router.get('/', (req, res, next) => {
-  res.render('index.js')
+// router.get('/', (req, res, next) => {
+//   res.render('index.pug')
+// })
+
+router.get('/new', req, res, next) => {
+  res.render('create.pug')
 })
