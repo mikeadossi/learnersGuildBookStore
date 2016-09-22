4e6ff106c5f673f1087770f21fe7c4f4db74ddee
James D Stewart <jamstewjr@gmail.com>
2016-09-22 10:34:12 -0700

    removed unhelpful comments

--- routes/search.js
+++ routes/search.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var db = require('../database');
 
-/* GET home page. */
 router.get('/', (req, res, next) => {
   const query = req.query
   db.findBooks(query)
