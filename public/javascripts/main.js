a39d8d1f0f91775c565fd2e4dc695b654819043d
mikeadossi <mikeadossi@gmail.com>
2016-09-22 17:48:19 -0700

    styled nav and page content, started work on js

---
+++ public/javascripts/main.js
@@ -0,0 +1,16 @@
+
+var str = document.querySelector('.descr');
+var strInner = str.innerHTML
+
+strInner.substring(0, Math.min(10,strInner.length))
+
+//if(strInner.length > 10) str = strInner.substring(0,10);
+var strTruncated = strInner.substring(0, Math.min(10,strInner.length));
+
+//str.update('strTruncated');
+//strInner.innerHTML = strTruncated
+
+str.length == 10;
+
+console.log('strInner.length:', strInner.length)
+console.log('strTruncated:',strTruncated)
